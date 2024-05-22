b10242413cdcbfa3b313736f3c66df71166f7cbc
David Silva <davidavilasilva@gmail.com>
2024-05-22 15:07:39 -0700

    Convenience thing for importing pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,4 @@
 export { default as ProductCreate } from "./ProductCreate";
+export { default as Landing } from "./Landing";
+export { default as Layout } from "./Layout";
+export { default as SignIn } from "./SignIn";
