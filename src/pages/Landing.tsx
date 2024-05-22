50772ccaf6c0518c070dbab68829441940850637
David Silva <davidavilasilva@gmail.com>
2024-05-22 15:09:45 -0700

    Add Landing page

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,13 @@
+import ListProducts from "../components/ListProducts";
+
+const Landing = () => {
+  return (
+    <div className="row">
+      <div className="col-12">
+        <h1>Landing Page</h1>
+        <ListProducts />
+      </div>
+    </div>
+  );
+};
+export default Landing;
