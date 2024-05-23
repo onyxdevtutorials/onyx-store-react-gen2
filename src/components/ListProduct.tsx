e938640505b466fe22492cdfbc6652e7fb5d63e8
David Silva <davidavilasilva@gmail.com>
2024-05-22 17:01:30 -0700

    Add a product update feature

--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -1,17 +1,33 @@
+import { useNavigate } from "react-router-dom";
 import { type Schema } from "../../amplify/data/resource";
+import Button from "react-bootstrap/Button";
 
 type Product = Schema["Product"]["type"];
 
 interface ListProductProps {
   product: Product;
+  isAdmin?: boolean;
 }
 
-const ListProduct: React.FC<ListProductProps> = ({ product }) => {
+const ListProduct: React.FC<ListProductProps> = ({ product, isAdmin }) => {
+  const navigate = useNavigate();
+
+  const handleUpdate = () => {
+    console.log("update", product);
+    navigate(`/admin/product-update/${product.id}`);
+  };
+
   return (
     <li>
       <div>{product.name}</div>
       <div>{product.description}</div>
       <div>{product.price}</div>
+      {isAdmin && (
+        <div>
+          <Button onClick={handleUpdate}>Edit</Button>
+          <Button>Delete</Button>
+        </div>
+      )}
     </li>
   );
 };
