1d9ab2f2c022d7d2c5e65f2fa71111846cf61206
David Silva <davidavilasilva@gmail.com>
2024-05-22 17:46:31 -0700

    Allow deleting of products

--- src/components/ListProduct.tsx
+++ src/components/ListProduct.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 import { type Schema } from "../../amplify/data/resource";
 import Button from "react-bootstrap/Button";
+import { generateClient } from "aws-amplify/api";
+import { useState } from "react";
 
 type Product = Schema["Product"]["type"];
 
@@ -9,13 +11,39 @@ interface ListProductProps {
   isAdmin?: boolean;
 }
 
+const client = generateClient<Schema>({
+  authMode: "userPool",
+});
+
 const ListProduct: React.FC<ListProductProps> = ({ product, isAdmin }) => {
   const navigate = useNavigate();
+  const [isDeleted, setIsDeleted] = useState<boolean>(false);
 
   const handleUpdate = () => {
     console.log("update", product);
     navigate(`/admin/product-update/${product.id}`);
   };
+
+  const handleDelete = async () => {
+    if (
+      window.confirm("Are you sure you want to delete this product?") === true
+    ) {
+      console.log("delete", product);
+      try {
+        const { data } = await client.models.Product.delete({
+          id: product.id,
+        });
+        setIsDeleted(true);
+        console.log("successfully deleted", data);
+      } catch (error) {
+        console.error("error", error);
+      }
+    }
+  };
+
+  if (isDeleted) {
+    return null;
+  }
 
   return (
     <li>
@@ -25,7 +53,7 @@ const ListProduct: React.FC<ListProductProps> = ({ product, isAdmin }) => {
       {isAdmin && (
         <div>
           <Button onClick={handleUpdate}>Edit</Button>
-          <Button>Delete</Button>
+          <Button onClick={handleDelete}>Delete</Button>
         </div>
       )}
     </li>
